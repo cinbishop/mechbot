c5d452dd0b2fb3072bcaf90b9d0f0d2b163cb180
Jacob Fox <jacob.jmf@gmail.com>
2018-06-16 01:39:16 -0400

    adding dependencies

--- mechbot.js
+++ mechbot.js
@@ -8,9 +8,31 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.channel.send('shut up');
+	if(msg.author.bot) {
+		return;
+	} else {
+		const prefix = "!";
+		const hasPrefix = msg.content.startsWith(prefix);
+		if(!hasPrefix) {
+			return;
+		} else {
+			var cmd = msg.content.match(/^!(jarl)\s(\S+)/);
+			console.log(cmd);
+			if(cmd) {
+				var oReq = new XMLHttpRequest();
+
+				function reqListener() {
+					console.log(this.responseText);
+				}
+
+				oReq.addEventListener('load',reqListner);
+				oReq.open("GET",'https://leadeboard.isengrim.org/api/usernames/'+username);
+				oReq.send();
+
+				//msg.channel.send('yup');
+			}
+		}
 	}
 });
 
-bot.login(authKey);
+bot.login(authKey.token);
