3a262e5ff6dba6711a1bfc43c4543bccf1f2a6ca
Jacob Fox <jacob.jmf@gmail.com>
2018-06-19 07:20:51 -0400

    adding smurfy command

--- mechbot.js
+++ mechbot.js
@@ -20,7 +20,7 @@ bot.on("message", msg => {
 		let season = args.join().split('-s')[1] || null;
 		let username = season === null ? args.join(' ') : args.slice(0,-1).join(' ');
 
-		let requestUrl = "https://leaderboard.isengrim.org/api/usernames/"+username;
+		let requestUrl = 'https://leaderboard.isengrim.org/api/usernames/'+username;
 		if(season != null) requestUrl += '/seasons/'+season;
 
 		https.get(requestUrl, (resp) => {
@@ -33,6 +33,7 @@ bot.on("message", msg => {
 			resp.on("end",() => {
 				let stats = '';
 				data = JSON.parse(data);
+				console.log(data);
 				for (var key in data) {
 					if(key === 'Rank' && data[key] === 0) {
 						data[key] = 'Retired';
@@ -43,6 +44,46 @@ bot.on("message", msg => {
 			});
 		}).on("error",(err) => {
 			console.log('Error: ' + err.message);
+		});
+	}
+
+	if(command === "smurfy") {
+		let requestedMech = args.toString().toLowerCase();
+		console.log(requestedMech);
+		let requestUrl = 'https://mwo.smurfy-net.de/api/data/mechs.json';
+
+		https.get(requestUrl, (resp) => {
+			let data = '';
+
+			resp.on("data",(chunk) => {
+				data += chunk;
+			});
+
+			resp.on("end",() => {
+
+				data = JSON.parse(data);
+				console.log(data[1]['name']);
+				//console.log(Object.keys(data['name']));
+
+				function findArr(arr, value) {
+					for (var i = 1, len = Object.keys(arr).length; i<len; i++) {
+						console.log(arr[i]);
+						if (arr[i]['name'] == value) return arr[i];
+					};
+					return false;
+				}
+
+				let selectedMech = findArr(data, requestedMech);
+				let hardpoints = '';
+				if(selectedMech) {
+					for (var key in selectedMech['details']['hardpoints']) {
+						hardpoints += '**'+key+'** ' + selectedMech['details']['hardpoints'][key] + '\n';
+					}
+					msg.channel.send(hardpoints);
+				}
+			}).on("error",(err) => {
+				console.log('Error: ' + err.message);
+			});
 		});
 	}
 });
