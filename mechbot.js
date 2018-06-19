b2f052159c1d2f625cff7b5e3c8e2d04fd2adfb1
Jacob Fox <jacob.jmf@gmail.com>
2018-06-18 23:02:18 -0400

    making suck less

--- mechbot.js
+++ mechbot.js
@@ -1,58 +1,50 @@
 const Discord = require("discord.js");
 const https = require("https");
-const authKey = require("./auth.json");
+const config = require("./auth.json");
 
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
+bot.on("ready", () => {
 	console.log('All systems nominal.');
 	bot.user.setActivity('!jarl [name] for epeen measurement', { type: 'PLAYING' });
 });
 
-bot.on('message', msg => {
-	if(msg.author.bot) {
-		return;
-	} else {
-		const prefix = "!";
-		const hasPrefix = msg.content.startsWith(prefix);
-		if(!hasPrefix) {
-			return;
-		} else {
-			var cmd = msg.content.match(/^!(jarl)\s([\w\s]+)(\s?-s\d{1,3})?/);
-			console.log(cmd);
-			if(cmd) {
-				var username = cmd[2].replace(/\s+$/,'').replace(' ','+');
-				var season = cmd[3] ? cmd[3].replace('-','').split('s')[1] : null;
-				var requestURLBase = "https://leaderboard.isengrim.org/api/usernames/"+username;
-				if(season != null) {
-					requestURLBase += '/seasons/'+season;
+bot.on("message", msg => {
+	if(msg.author.bot) return;
+	if(msg.content.indexOf(config.prefix) !== 0) return;
+
+	const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if(command === "jarl") {
+		let season = args.join().split('-s')[1] || null;
+		let username = season === null ? args.join(' ') : args.slice(0,-1).join(' ');
+
+		let requestUrl = "https://leaderboard.isengrim.org/api/usernames/"+username;
+		if(season != null) requestUrl += '/seasons/'+season;
+
+		https.get(requestUrl, (resp) => {
+			let data = '';
+
+			resp.on("data",(chunk) => {
+				data += chunk;
+			});
+
+			resp.on("end",() => {
+				let stats = '';
+				data = JSON.parse(data);
+				for (var key in data) {
+					if(key === 'Rank' && data[key] === 0) {
+						data[key] = 'Retired';
+					}
+					stats += '**'+key+'** ' + data[key] + '\n';
 				}
-				/*! LOOKUP FULL USER STATS **/
-				https.get(requestURLBase, (resp) => {
-					let data = '';
-
-					resp.on('data',(chunk) => {
-						data += chunk;
-					});
-
-					resp.on('end', () => {
-						var message = "";
-						//console.log(JSON.parse(data));
-						data = JSON.parse(data);
-						for (var key in data) {
-							if(key === "Rank" && data[key] === 0) {
-								data[key] = "RETIRED";
-							}
-							message += '**'+key+':** ' + data[key] + '\n'; 
-						}
-						msg.channel.send(message);
-					});
-				}).on("error",(err) => {
-					console.log("Error: " + err.message);
-				});
-			}
-		}
+				msg.channel.send(stats);
+			});
+		}).on("error",(err) => {
+			console.log('Error: ' + err.message);
+		});
 	}
 });
 
-bot.login(authKey.token);
+bot.login(config.token);
