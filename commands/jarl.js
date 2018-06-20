47bf5f8fa563d7031f7ebf17813c9bf26f6f0d0e
Jacob Fox <jacob.jmf@gmail.com>
2018-06-20 00:34:32 -0400

    updating structure

---
+++ commands/jarl.js
@@ -0,0 +1,31 @@
+exports.run = (client, message, args) => {
+	//console.log(args);
+	let season = args.join().split('-s')[1] || null;
+	let username = season === null ? args.join(' ') : args.slice(0,-1).join(' ');
+
+	let requestUrl = 'https://leaderboard.isengrim.org/api/usernames/'+username;
+	if(season != null) requestUrl += '/seasons/'+season;
+
+	client.https.get(requestUrl, (resp) => {
+		let data = '';
+
+		resp.on("data",(chunk) => {
+			data += chunk;
+		});
+
+		resp.on("end",() => {
+			let stats = '';
+			data = JSON.parse(data);
+			//console.log(data);
+			for (var key in data) {
+				if(key === 'Rank' && data[key] === 0) {
+					data[key] = 'Retired';
+				}
+				stats += '**'+key+'** ' + data[key] + '\n';
+			}
+			message.channel.send(stats);
+		});
+	}).on("error",(err) => {
+		console.log('Error: ' + err.message);
+	});
+}
